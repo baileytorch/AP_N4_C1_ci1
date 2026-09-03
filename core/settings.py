90b60b218ecdf65587abda817e1d924108b381fb
baileytorch <baileytorch@hotmail.com>
2026-09-02 20:20:08 -0400

    Actualizando vistas

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+*)%2x=s)y7s%@5(obnzr4sxt1j3ep26+x4tfdy#wsxuxgljqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
