3021c6f674d24eb0567c19a3fbf3a8c377fc6e79
Erick Bailey <baileytorch@hotmail.com>
2026-09-10 20:23:28 -0300

    Actualizando modelo

--- taller/models.py
+++ taller/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Estado(models.Model):
+    estado = models.CharField(max_length=25)
+    descripcion = models.CharField(max_length=100)
+
+class Ubicacion(models.Model):
+    ubicacion = models.CharField(max_length=50)
+    detalles = models.CharField(max_length=100)
+
+class Inventario(models.Model):
+    estado = models.ForeignKey(Estado,on_delete=models.CASCADE)
+    ubicacion = models.ForeignKey(Ubicacion,on_delete=models.CASCADE)
